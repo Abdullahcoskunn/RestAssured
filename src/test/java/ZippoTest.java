b2185337da4c32693f803e799805ba5fed066acc
Abdullahcoşkunn <sensiz.coskun.71@gmail.com>
2021-08-24 21:20:37 +0300

    Initial commit

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class ZippoTest {
+    @Test
+    public void test(){
+        given()
+
+                //hazırlık işlemleri
+                .when()
+                //link ve aksiyon işlemlerri
+
+                .then()
+        //test ve extract işlemleri
+
+        ;
+
+    }
+}
